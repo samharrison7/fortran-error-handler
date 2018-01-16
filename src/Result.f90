fa887aca829c94a12db300161a284fc80123b83e
Sam Harrison <samharrison.xg@gmail.com>
2018-01-16 15:56:47 +0000

    Making kind parameters private so as not to conflict.

--- src/Result.f90
+++ src/Result.f90
@@ -7,8 +7,8 @@ module ResultModule
     ! Set kind parameters for use with real variables, as per the convention
     ! recommended in, e.g., http://fortranwiki.org/fortran/show/Real+precision
     ! and N. S. Clerman and W. Spector, Modern Fortran (2012).
-    integer, parameter :: dp = selected_real_kind(15,307)       !! Double precision, 15 digits, 1e307
-    integer, parameter :: qp = selected_real_kind(33,4931)      !! Quadruple precision, 33 digits, 1e4931
+    integer, parameter, private :: dp = selected_real_kind(15,307)       !! Double precision, 15 digits, 1e307
+    integer, parameter, private :: qp = selected_real_kind(33,4931)      !! Quadruple precision, 33 digits, 1e4931
 
     !> The `Result` type is an addon to the framework and is designed as an object
     !! to be returned from any procedures that may throw an error. It consists of
